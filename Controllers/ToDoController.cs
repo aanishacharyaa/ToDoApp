8778f555e84841937e56659125c6162e427aedb2
aa <aa@DESKTOP-JSB87OQ>
2023-04-12 23:18:15 -0400

    Add project files.

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,80 @@
+﻿        using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Data;
+using ToDoApp.Migrations;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ToDoDatabase _database;
+
+        public ToDoController(ToDoDatabase db)
+        {
+            _database = db;
+        }
+
+        public async Task<IActionResult> Index()
+        { 
+            var todos = await _database.ToDos.ToListAsync();
+            return View(todos);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Add(string newItem) 
+        {
+            if(newItem != null) {
+                var todo = new ToDoItem
+                {
+                    Title = newItem,
+                    IsComplete = false
+                };
+                _database.ToDos.Add(todo);
+                await _database.SaveChangesAsync();
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Complete(IEnumerable<int> isComplete)
+        {
+            if(isComplete != null)
+            {
+                foreach (var item in isComplete)
+                {
+                    var todo = _database.ToDos.FirstOrDefault(t => t.Id == item);
+                    if(todo != null) {
+                        todo.IsComplete = true;
+                    }
+                }
+                _database.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteAll()
+        {
+            _database.ToDos.RemoveRange(_database.ToDos);
+            _database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var todo = _database.ToDos.FirstOrDefault(t => t.Id == id);
+            if (todo != null)
+            {
+                _database.ToDos.Remove(todo);
+                _database.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ToDoDatabase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.IO;
+using ToDoApp.Models;
+
+namespace ToDoApp.Data
+{
+    public class ToDoDatabase : DbContext
+    {
+        public string DbPath { get; }
+
+        public ToDoDatabase(DbContextOptions<ToDoDatabase> options) : base (options)
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source=ToDo.db");
+
+        public DbSet<ToDoItem> ToDos { get; set; }
+
+    }
+
+    
+}
